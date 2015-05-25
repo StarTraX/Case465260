85c8930011695cbf5a6c89fc482c0958ac986724
StarTraX <info@gpsanimator.com>
2015-05-25 10:58:59 +1000

    initial commit
    Building reproducible case for Pebble support: crashes with inexplicable error.

---
+++ src/nav_next_mark.c
@@ -0,0 +1,111 @@
+#include "nav_next_mark.h"
+#include <pebble.h>
+#include "dashboard.h"
+#include "nav_mark_menu.h"
+# define NEXTMARKCOUNT 12
+static Window *s_window;
+ static ScrollLayer *scroll_layer;
+//static ActionBarLayer *action_bar;
+static int dispList [NEXTMARKCOUNT] = {
+	GPSTIME,
+	WPTNAME,
+	WPTDISPDIST,
+	LAYLINEHDG,//Heading "Lay-line"
+	LAYLINEDIST,
+	LAYLINETIME,
+	BRGCLOCK,
+	BRGDEGS,
+	WPTVMG,
+	WPTBRGMAG,
+	WPTETI,
+	WPTETA	};
+//char  Msg[100];
+
+static void scroll_up(){
+	//APP_LOG(APP_LOG_LEVEL_DEBUG, "scroll_up detected");
+	GPoint scrollPoint = scroll_layer_get_content_offset(scroll_layer);
+	if (scrollPoint.y < 0)scrollPoint.y +=30;
+	scroll_layer_set_content_offset(scroll_layer,scrollPoint,true);
+}
+static void scroll_down(){
+	//APP_LOG(APP_LOG_LEVEL_DEBUG, "scroll_down detected");
+	GPoint scrollPoint = scroll_layer_get_content_offset(scroll_layer);
+	scrollPoint.y -=30;
+	scroll_layer_set_content_offset(scroll_layer,scrollPoint,true);
+}
+static void config_provider() {
+  // window_single_click_subscribe(BUTTON_ID_SELECT, show_nav_divs_menu);
+	if (intRole==0)
+		window_single_click_subscribe(BUTTON_ID_SELECT, show_nav_mark_menu);
+
+}
+char Msg[60];
+static void add_text_layer( int dispIdx ){
+	displayFields[dispIdx] = text_layer_create(GRect(0, rowSpace*(rowIndex++), 126, 40)); 
+  	text_layer_set_font( displayFields[dispIdx],displayFont1);
+  	scroll_layer_add_child(scroll_layer, (Layer *)displayFields[dispIdx]);
+}
+static void window_load(Window *window) {
+	rowIndex=0;
+	rowSpace = 32;
+	text_layer_set_text(page_heading, "--- NEXT MARK ---");
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(page_heading));
+					
+ 	GRect max_text_bounds = GRect(0, 26, 144, 168); //TODO parameterise scroll-window height
+ 	scroll_layer = scroll_layer_create(max_text_bounds);  // size of the scroll layer
+  	scroll_layer_set_click_config_onto_window(scroll_layer, window);
+  	scroll_layer_set_content_size(scroll_layer, GSize(144,500)); // size of the surface that scrolls???
+	scroll_layer_set_callbacks(scroll_layer, (ScrollLayerCallbacks){
+		.click_config_provider= &config_provider,
+	});
+	for (int i =0; i <NEXTMARKCOUNT; i++){
+		 	add_text_layer(dispList[i]); //creates the text layer and adds to the scroll layer
+	}
+
+ 
+	layer_add_child(window_get_root_layer(window), scroll_layer_get_layer(scroll_layer));
+	//scroll_layer_set_callbacks(scroll_layer, (ScrollLayerCallbacks){
+	//	.click_config_provider = &config_provider	
+	//});
+	/*
+	 	action_bar = action_bar_layer_create();
+ 	action_bar_layer_add_to_window(action_bar, window);
+	//action_bar_layer_set_icon(action_bar, BUTTON_ID_UP,gbitmap_create_with_resource(RESOURCE_ID_BACK_ICON));
+	//action_bar_layer_set_icon(action_bar, BUTTON_ID_DOWN,gbitmap_create_with_resource(RESOURCE_ID_FWD_ICON));
+	action_bar_layer_set_icon(action_bar, BUTTON_ID_SELECT,gbitmap_create_with_resource(RESOURCE_ID_START_ICON));
+ 	//action_bar_layer_set_click_config_provider(action_bar, &config_provider);
+*/
+}
+static void handle_nav_window_unload(Window* window) {
+	for (int i =0; i <NEXTMARKCOUNT; i++){
+		text_layer_destroy(displayFields[dispList[i]]);
+		displayFields[dispList[i]] = NULL;
+	}
+	//text_layer_destroy(page_heading);
+	scroll_layer_destroy(scroll_layer);
+  	window_stack_remove(window, true);
+  	window_destroy(window);
+
+
+}
+ static void window_appear(){
+		text_layer_set_text(displayFields[dispList[1]], refreshingMsg);
+	 	send_to_phone(TupletCString(100, "nav_next_mark"));
+
+}
+void show_nav_next_mark(void) {
+
+	 s_window = window_create();
+  //	window_set_fullscreen(s_window, true);	
+
+  window_set_window_handlers(s_window, (WindowHandlers) {
+	  .load = window_load,
+    .unload = handle_nav_window_unload,
+	  .appear = window_appear,
+  });
+  window_stack_push(s_window, true);
+	//snprintf(Msg, 100,  "window_stack_pushheap free: %d ", heap_bytes_free());
+ 	//APP_LOG(APP_LOG_LEVEL_INFO, Msg);
+
+}
+
---
+++ src/pebble-js-app.js
@@ -0,0 +1,192 @@
+/* jshint -W099 */ //remove warning about mixed spaces and tabs???
+var WEB_HOST ; //= "http://192.168.0.6:8080/dashboard/";	
+Pebble.addEventListener("ready",load_data);
+
+Pebble.addEventListener("showConfiguration",
+  function(e) {
+	  console.log("showConfiguration: ");
+	var pebbleConfigURL = "http://gpsanimator.com/tactician/pebbleConfig";
+	Pebble.openURL(pebbleConfigURL);
+	}
+); //addEventListener showConfiguration
+var settings = {};	
+
+Pebble.addEventListener('webviewclosed',					
+  function(e) {// calls configData.php which unpacks the config data and writes them to the phone's local storage
+	if (e.response != "cancel"){
+		var configObj = JSON.parse(e.response); // it's assumed to be a JSON object
+		settings = configObj.settings; //"yachtLength":"12","gpsBehindBow":"8","perfStyle":"2","divIdx":"0","startingPolars":"startingPolars.txt","racingPolars":"racingPolars.txt","clubIdx":0,"seriesIdx":"4","PHPSESSID":"b26254b83b75dbd4f803d6adaddc3b89"}
+		//console.log("Settings: "+JSON.stringify(settings));	  
+		localStorage.settings = JSON.stringify(settings); // available for next login			
+		if (configObj.settings.role=="admin"){ // this has to be here, not in the gpsanimator.com/pebbleConfig script  
+			var configData = configObj.configData;
+			var http = new XMLHttpRequest(); // writing stuff for Logger: userName, seq, dongle MacAddress
+			var mURL = "http://localhost:8080/dashboard/configData.php";
+			http = new XMLHttpRequest(); //for navStart.php		
+			var params = "configData="+encodeURI(JSON.stringify(configData));
+			http.open("POST", mURL, true);
+			http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			http.setRequestHeader("Content-length", params.length);
+			http.setRequestHeader("Connection", "close");
+			http.send(params);
+			http.onreadystatechange = function () {
+				if (http.readyState == 4 && http.status == 200 ){
+						//console.log ("configData response: "+http.responseText);
+					} // if readyState == 4
+				}; //onreadystatechange	  
+			}
+ 		load_data();
+	} // if not cancel
+}//anonymous callback function
+	  
+    //Pebble.showSimpleNotificationOnPebble('Configuration window returned: ');
+);
+var watchPhase, selectedWindow, presentPosData, startLinePoints;
+Pebble.addEventListener("appmessage",						
+	 function(e) {
+		 var mURL;
+		 var mRequest = new XMLHttpRequest(); 
+		for (var msgIdx in e.payload ){
+			if (msgIdx == 100){ // selected WINDOW 
+				selectedWindow = e.payload[msgIdx];
+				//console.log("received: "+selectedWindow);
+				if (selectedWindow.substr(0,5)=="start" )
+					watchPhase = "start";
+				else if (selectedWindow.substr(0, 4) =="perf" )
+					watchPhase = "perf";
+				else if (selectedWindow.substr(0, 3)=="nav" )
+					watchPhase = "nav";
+			}
+			//console.log("watchPhase: "+watchPhase);
+			if (msgIdx == 101){// selected course
+			set_course(e.payload[msgIdx]);
+			}
+			else if (msgIdx == 102){// from next_mark window
+				
+				if ( e.payload[msgIdx] === 0){
+					console.log("Next_mark : manual start");
+					mURL =  WEB_HOST + "startNav.php";
+					mRequest = new XMLHttpRequest(); //for navStart.php
+					mRequest.open("GET", mURL, true); 
+					mRequest.send(null);
+					mRequest.onreadystatechange = function () {
+						  if (mRequest.readyState == 4 && mRequest.status == 200 ){
+						   } // if readyState == 4
+					}; //onreadystatechange
+				}
+				else {
+					mURL =  WEB_HOST + "markFwdBack.php?indicator="+e.payload[msgIdx];
+					//console.log("sending: "+mURL );
+					mRequest.open("GET", mURL, true); 
+					mRequest.send(null);	
+					mRequest.onreadystatechange = function () {
+							if (mRequest.readyState == 4 && mRequest.status == 200 ){
+								var resp = mRequest.responseText;	
+							  	console.log ("markFwdBack.php: with "+  e.payload[msgIdx] + " returned with "+resp);
+						   } // if readyState == 4
+						}; //onreadystatechange	
+				}
+			}
+			else if (msgIdx == 103){// from start_line
+				//console.log ("start_line  with "+  e.payload[msgIdx] );
+				mURL = WEB_HOST +"startLinePress.php?whichEnd="+  e.payload[msgIdx]; // Updates & gets location data string
+				mRequest.open("GET", mURL, true);
+				mRequest.send(null);
+				mRequest.onreadystatechange = function () {
+					if (mRequest.readyState == 4 ){
+						if(mRequest.status == 200){
+						} // if status == 200
+					} // if readyState == 4
+				}; //onreadystatechange
+				if (e.payload[msgIdx] == "BOAT") print_division();  //refresh course wirh new start line (bot)
+			}
+			else if (msgIdx == 104){// Start time 10, 5, 4 mins, as minutes
+				startTime = Number(presentPosData.timeMs) + Number(e.payload[msgIdx]) *1000 ; // to thous
+				mURL = WEB_HOST +"startLinePress.php?startTime="+  startTime; // Updates & gets location data string
+				mRequest.open("GET", mURL, true);
+				mRequest.send(null);
+				mRequest.onreadystatechange = function () {
+					if (mRequest.readyState == 4 ){
+						if(mRequest.status == 200){
+						} // if status == 200
+					} // if readyState == 4
+				}; //onreadystatechange
+			}
+			else if (msgIdx == 105){//  selected course PLUS BRG to mark(s)					
+				set_courseWithBrg(e.payload[msgIdx]);
+			}
+		}
+});  //Pebble.addEventListener("appmessage",	
+						
+function set_course(courseIdx){
+	//console.log("set_course: "+courseIdx);
+	var url = WEB_HOST + "pebbleGetCourses.php?set_course="+courseIdx; 
+	var set_courseRequest = new XMLHttpRequest(); 
+	set_courseRequest.open("GET", url, true);	
+	set_courseRequest.send(null);
+	set_courseRequest.onreadystatechange = function () {
+		  if (set_courseRequest.readyState == 4 ){
+			   	if(set_courseRequest.status == 200){ // or 404 not found	
+					print_division(); //refresh 
+				}				
+			  else {
+				  setTimeout(function(){set_course(courseIdx);},1000);
+			  }
+		}
+	};								  										  
+}
+function set_courseWithBrg(idxPlusBrg){
+	//console.log("set_courseWithBrg: "+idxPlusBrg);
+	var url = WEB_HOST + "pebbleGetCourses.php?set_courseWithBrg="+idxPlusBrg; 
+	var http = new XMLHttpRequest(); 
+	http.open("GET", url, true);	
+	http.send(null);
+	http.onreadystatechange = function () {
+		  if (http.readyState == 4 ){
+			   	if(http.status == 200){ // or 404 not found	
+					print_division(); //refresh 					
+				}				
+			  else {
+				  setTimeout(function(){set_course(idxPlusBrg);},1000);
+			  }
+		}
+	};								  										  
+}
+
+var httpFlags = 0; //1: polars, 2, division, 4:all course, 8: current course
+var readPolarsRequest = new XMLHttpRequest(); //for read Polars data
+var targets, starting; // objects holding racing and starting [polars]
+function readPolars(){
+	readThesePolars("racing");
+}
+function readThesePolars(whichPolars){
+	//Pebble.showSimpleNotificationOnPebble("readPolars",whichPolars);
+	var url = WEB_HOST + "pebbleGetPolars.php?"+ whichPolars ; // gets polars in a json string
+	readPolarsRequest.open("GET", url, true);
+	readPolarsRequest.send(null);
+	readPolarsRequest.onreadystatechange = function () {
+		  if (readPolarsRequest.readyState == 4 ){
+			   	if(readPolarsRequest.status == 200){ // or 404 not found
+				   	var mData = readPolarsRequest.responseText;
+					//Pebble.showSimpleNotificationOnPebble("readPolarsRequest.responseText",readPolarsRequest.responseText);
+				   	if (mData.length >0){
+						var polarArray = JSON.parse(readPolarsRequest.responseText);
+						if (whichPolars=="racing")
+							targets = new polarClass(polarArray);
+							//Pebble.showSimpleNotificationOnPebble("targets.UpwindTWAs[1]: ",targets.upwindTWAs[1]);
+						else if (whichPolars=="starting"){
+							starting = new polarClass(polarArray);
+						}
+					}// length > 0	
+					if (whichPolars=="racing")
+						readThesePolars("starting");
+					else if (whichPolars=="starting")
+						testHttpFlags(1);
+				}//status == 200
+			  	else 
+				  Pebble.showSimpleNotificationOnPebble("HTTP Fail(1)","Check your web server is running");
+			} //readyState == 4
+		   }; //onreadystatechange 		
+}
+
+
